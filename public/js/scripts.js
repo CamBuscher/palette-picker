2cddc3683b64b7b1ab890259111368ccfa5d27fa
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-26 15:05:27 -0600

    Boxes can now randomly generate colors, and only on boxes that are not locked.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,52 @@
 $(document).ready(() => {
-  console.log('yo')
+  
+  const boxes = {
+    box1 : {
+      'locked': true,
+      'color': '#000000'
+    },
+    box2 : {
+      'locked': false,
+      'color': null
+    },
+    box3 : {
+      'locked': false,
+      'color': null
+    },
+    box4 : {
+      'locked': false,
+      'color': null
+    },
+    box5 : {
+      'locked': false,
+      'color': null
+    }
+  }
 
+  function appendBox(box, color) {
+    const style = 'background-color:' + color
+    $('.colors-container').append(`
+      <div class='color-box' class=${box} style=${style}>
+        <p>${color}</p>
+      </div>
+    `)
+  }
+
+  const hexGenerator = () => '#' + ('000000' + (Math.random() * 0xFFFFFF << 0).toString(16)).slice(-6)
+
+  function generateColors() {
+    $('.colors-container').html(`<div></div>`)
+    Object.keys(boxes).forEach(box => {
+      console.log(box)
+      if (boxes[box].locked) {
+        appendBox(box, boxes[box].color) 
+      } else {
+        const color = hexGenerator()
+
+        appendBox(box, color)
+      }
+    })
+  }
+
+  generateColors()
 })
