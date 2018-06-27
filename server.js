a751d4ea6a41c34727f62a6816ccd70debd59635
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-27 16:05:29 -0600

    Add palette post method

--- server.js
+++ server.js
@@ -47,10 +47,30 @@ app.post('/api/v1/projects', (request, response) => {
   }
 
   database('projects').insert(project, 'id')
-    .then(project => {
-      response.status(201).json({ id: project[0] })
+    .then(project_id => {
+      response.status(201).json({ id: project_id[0] })
     })
     .catch(error => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body;
+
+  for (let requiredParameter of ['name', 'color1', 'color2', 'color3', 'color4', 'color5', 'project_id']) {
+    if (!palette[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String>, project_id: <Integer> }. You're missing a "${requiredParameter}" property.` });
+    }
+  }
+
+  database('palettes').insert(palette, 'id')
+    .then(palette_id => {
+      response.status(201).json({ id: palette_id[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
