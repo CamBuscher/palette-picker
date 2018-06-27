a3a113a8df00b2d1e5757973dc35a87f36d60712
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-26 22:19:06 -0600

    Mess around with post request to no avail

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -24,6 +24,7 @@ $(document).ready(() => {
   }
 
   $('.generate').on('click', generateColors)
+  $('.project-form').on('submit', createNewProject)
 
   function appendBox(box, color) {
     const style = 'background-color:' + color
@@ -39,15 +40,42 @@ $(document).ready(() => {
   function generateColors() {
     $('.colors-container').html(`<div></div>`)
     Object.keys(boxes).forEach(box => {
-      console.log(box)
       if (boxes[box].locked) {
         appendBox(box, boxes[box].color) 
       } else {
         const color = hexGenerator()
-
         appendBox(box, color)
       }
     })
+  }
+
+  function createNewProject(e) {
+    e.preventDefault();
+    const project = $('.project-input').val()
+
+    // var settings = {
+    //   "async": true,
+    //   "crossDomain": true,
+    //   "url": "http://localhost:3000/api/v1/projects/new",
+    //   "method": "POST",
+    //   "headers": {
+    //     "Content-Type": "application/json",
+    //     "Cache-Control": "no-cache",
+    //   },
+    //   "processData": false,
+    //   "data": "{\n\t\"Content-Type\": \"application/json\",\n\t\"project\": \"palette-picker\"\n}"
+    // }
+
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:3000/api/v1/projects/new",
+      data: {
+        project,
+        "Content-Type": "application/json"
+      }
+    });
+ 
+    $('.project-input').text('')
   }
 
   generateColors()
