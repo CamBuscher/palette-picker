06d15f1d1037d36ff97c408b3f974ef6b30024c8
Cameron Buscher <cameronbuscher@gmail.com>
2018-06-27 15:11:06 -0600

    Add data seeding for knex

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
